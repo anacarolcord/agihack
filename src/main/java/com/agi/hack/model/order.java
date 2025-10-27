bed02ba698f61a9d0cf8dae2106b9dcfe757443a
jaohab <113658271+jaohab@users.noreply.github.com>
2025-10-27 15:03:31 -0300

    Add equipmentList enum and order entity
    Introduces the equipmentList enum for equipment types and the order entity class with JPA annotations, including fields for order date, expected date, and equipment type.

---
+++ src/main/java/com/agi/hack/model/order.java
@@ -0,0 +1,74 @@
+package com.agi.hack.model;
+
+import java.time.LocalDateTime;
+
+import com.agi.hack.enums.equipmentList;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime orderDate;
+
+    private LocalDateTime expectedDate;
+
+    @Enumerated(EnumType.STRING)
+    private equipmentList Type;
+
+    public order() {
+    }
+
+    public order(Long id, LocalDateTime orderDate, LocalDateTime expectedDate, equipmentList type) {
+        this.id = id;
+        this.orderDate = orderDate;
+        this.expectedDate = expectedDate;
+        Type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDateTime getExpectedDate() {
+        return expectedDate;
+    }
+
+    public void setExpectedDate(LocalDateTime expectedDate) {
+        this.expectedDate = expectedDate;
+    }
+
+    public equipmentList getType() {
+        return Type;
+    }
+
+    public void setType(equipmentList type) {
+        Type = type;
+    }
+
+    
+    
+}
