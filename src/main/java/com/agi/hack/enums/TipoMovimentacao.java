e9301b34bca6b09dcada9a76402b49888a40cccc
Gustavo Andrade Magalhães <gustavomagalhaes0516@gmail.com>
2025-10-27 15:51:16 -0300

    Movimentacao e tipoMovimentacao

--- src/main/java/com/agi/hack/enums/TipoMovimentacao.java
+++ src/main/java/com/agi/hack/enums/TipoMovimentacao.java
@@ -4,5 +4,5 @@ public enum TipoMovimentacao {
     ENTRADA,
     SAIDA,
     TRANSFERENCIA,
-    AJUSTE
+
 }
--- src/main/java/com/agi/hack/repository/MovimentacaoRepository.java
+++ src/main/java/com/agi/hack/repository/MovimentacaoRepository.java
@@ -2,5 +2,5 @@ package com.agi.hack.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovimentacaoRepository  extends JpaRepository<Movimento, Long> {
+public interface MovimentacaoRepository  extends JpaRepository<MovimentacaoRepository, Long> {
 }
